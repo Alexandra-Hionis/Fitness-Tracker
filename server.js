395404d6c6e2aa9a4cb951c7863633ee0a6655b3
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-01-08 16:35:52 -0500

    Fixed error by deleted (app) of off require(./routes/api-routes.js)(app); in server.js. New error now path not defined server.js:34:18. Also added some express notes hehe

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
 //   console.log(`App running on port ${PORT}!`);
 // });
 
-require("./routes/api-routes.js")(app);
+app.use(require("./routes/api-routes.js"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
@@ -42,5 +42,25 @@ app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
 
-// ReferenceError: path is not defined
-//     at /Users/alexandrahionis/repos/Fitness-Tracker/server.js:34:18
+
+
+// notes to self 
+
+// So a lot of “stuff” goes on when establishing routes and making http requests….
+// Express is middleware that handles most of that stuff internally and with a lot less code…
+// So, you are telling your app, “Hey, use Express for this” with const app = express()…
+
+// Then you are adding middleware or built-in functions to say, “Hey, and make sure everything is urlencoded and bring my responses back in json form….
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// You are also saying, “When the program comes looking for my html, css, and images and stuff look for it in my public folder and start the file paths from their.
+
+// Hence….app.use(express.static("public"));
+
+// And then you are setting up your routes to be located at different endpoints by using app.use("./whatever/route/you/want")
+
+// app.use… is kinda like picking out a new car and deciding what all the features that you want built into it.
+
+// This way all these things get handled in the middle of 1.Making the http request and 2. Sending the http request.
