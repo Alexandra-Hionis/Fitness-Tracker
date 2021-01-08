08b1afa2c3f6417b64632c938ae3a75cc200b460
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-01-08 16:04:53 -0500

    Got server to connect without issues. Had to add { useUnifiedTopology: true } to the mangoclient constructor. This got rid of the error. Connected routes to front end html files

--- server.js
+++ server.js
@@ -10,15 +10,34 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
-app.use(require("./routes/api-routes.js"));
+// app.use(require("./routes/api-routes.js"));
+
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
+
+require("./routes/api-routes.js");
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
-getting-started.js
