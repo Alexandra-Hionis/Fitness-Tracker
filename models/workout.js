9ebf29f700ca91cde3bbaeb59be0cd73a9c14e95
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-01-08 23:51:09 -0500

    changed workout.js back to what was previously there

--- models/workout.js
+++ models/workout.js
@@ -55,7 +55,7 @@ const workoutSchema = new Schema({
   
 });
 
-const Workout = mongoose.model("workouts", workoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
 
