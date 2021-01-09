5057b9d3725b1312ef26be8a9cd3273a51e685f4
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-01-08 23:23:45 -0500

    fixed server.js. Reverted it back to how it was before because it wasn't loading the page properly

--- server.js
+++ server.js
@@ -12,21 +12,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true
-// });
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Workouts',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
 // routes
 
 require('./routes/api-routes.js')(app);
